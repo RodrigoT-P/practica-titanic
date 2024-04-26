8a632b7cf4779d270d79c6f4d781766b81aa3b8a
RodrigoT-P <a01703521@tex.mx>
2024-04-25 20:14:56 -0600

    matplotlib

--- hola.py
+++ hola.py
@@ -8,9 +8,9 @@ titanic_link = "titanic.csv"
 titanic_data = pd.read_csv(titanic_link)
 st.dataframe(titanic_data)
 
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
-#fig, ax = plt.subplots()
-#ax.hist(titanic_data.Fare)
-#st.header("Histograma del Titanic")
-#st.pyplot(fig)
+fig, ax = plt.subplots()
+ax.hist(titanic_data.Fare)
+st.header("Histograma del Titanic")
+st.pyplot(fig)
